d4fb8f8595beda1acbc4e87131e68d82f0a6f372
Matthew Underwood <legoshrimp@gmail.com>
2015-05-16 18:04:14 -0700

    added info on plans set up some functions

--- schema.sql
+++ schema.sql
@@ -1,66 +1,29 @@
-/*Project Phase 2                         CS 166 Database Management Systems  
-Project Group #29
-Matthew Underwood                       Instructor: V.Tsotras
-ID 861-04-4121                      Section 22
-munde001@ucr.edu                    TA: Karishma Dash
-Jordan Meyer                            Instructor: V.Tsotras
-ID 861-01-5184                      Section 21
-jmeye006@ucr.edu                    TA: Samarth
-*/
+drop table if exists champions;
+drop table if exists game;
+drop table if exists champ_game
+create table champions ( 
+	id integer primary key,
+    role text,
+	name text,
+	win integer,
+	loss integer,
+    total integer
 
-
-DROP TABLE User;
-DROP TABLE Message;
-DROP TABLE EducationDetails;
-DROP TABLE WorkExperience;
-DROP TABLE Connection;
-
-CREATE TABLE User
-(userid CHAR(10) NOT NULL UNIQUE,
-    name CHAR(50),
-    password CHAR(10) NOT NULL,
-    email CHAR(50) NOT NULL,
-    dateofbirth DATE,
-    PRIMARY KEY userid );
-
-CREATE TABLE Message
-(messageid INTEGER NOT NULL AUTO_INCREMENT,
-    senderid CHAR(10) NOT NULL,
-    receiverid CHAR(10) NOT NULL,
-    contents TEXT NOT NULL,
-    sendtime TIMESTAMP NOT NULL,
-    deletestatus INTEGER,
-    status CHAR(30) NOT NULL,
-    PRIMARY KEY messageid,
-    FOREIGN  KEY senderid REFERENCES User,
-    FOREIGN  KEY receiverid REFERENCES User  );
-
-
-CREATE TABLE EducationDetails
-(userid CHAR(10) NOT NULL,
-    institutionname CHAR(50) NOT NULL,
-    major CHAR(50) NOT NULL,
-    degree CHAR(50) NOT NULL,
-    startdate DATE,
-enddate DATE,
-PRIMARY KEY (userid, major, degree),
-FOREIGN  KEY userid REFERENCES User );
-
-CREATE TABLE WorkExperience
-(userid CHAR(10) NOT NULL,
-    company CHAR(50) NOT NULL,
-    role CHAR(50) NOT NULL,
-    location CHAR(50),
-    startdate DATE NOT NULL,
-enddate DATE,
-PRIMARY KEY (userid, company, role, startdate), 
-FOREIGN  KEY userid REFERENCES User );
-
-CREATE TABLE Connection
-(userid CHAR(10) NOT NULL,
-    connectionid CHAR(10) NOT NULL,
-    status CHAR(30) NOT NULL,
-    PRIMARY KEY (connectionid, userid),
-    FOREIGN  KEY userid REFERENCES User,
-    FOREIGN  KEY connectionid REFERENCES User.userid);
-
+);
+create table champ_game (
+    gameid integer primary key,
+    --team 1 blue
+    champ1 integer,
+    champ2 integer,
+    champ3 integer,
+    champ4 integer,
+    champ5 integer,
+    --team 2 red
+    champ6 integer,
+    champ7 integer,
+    champ8 integer,
+    champ9 integer,
+    champ10 integer,
+    win boolean --0 team 1 win, 1 team 2 win
+    );
+     
--- server.py
+++ server.py
@@ -4,7 +4,37 @@ import json
 import os
 w = RiotWatcher('88a2054a-5234-4303-880d-b5d7028f7ca0')
 app = Flask(__name__)
+#will need functions for:
+#def build_champ_db that is indexable by  their id to avoid constantly querying the static db
+#Constants
+#team types
+COMP_UNKNOWN = "unknown"
+#standard team types 
+#COMP_AP
+#MID:AP carry or AP assassin
+#BOT: ADC and support
+COMP_AP = "ap"
+#COMP_AD
+
+#COMP_AP_SWAP
+#COMP_AD_SWAP
+
+#support variance
+#COMP_SUP_NONE
+#COMP_SUPDAMAGE --built 1+ dedicated damage items.
+#COMP_
 #champions = w.static_get_champion_list()
+
+
+#player info
+#lane position
+# number shared with
+# main/shared/no farm 
+# damage type
+# role [asssassin/bruiser/burst/carry/zerocs/tank/???]
+# supporty [y/n]
+# roamer [y/n]
+
 @app.route('/hello/<name>')
 def hello(name=None):
 	if name != None:
@@ -20,17 +50,44 @@ def hello(name=None):
 		return "no user input"
 @app.route('/')
 def index(): 
-    process_seed_data()
+    with open('matches1.json') as json_file:
+        matches = json.load(json_file, 'latin-1')
+    process_seed_data(matches)
 
 
 
 @app.route('/user/<username>')
 def profile(username): pass
+class champion_info:
+    #needs to have:
+    #Champion name
+    #Champion role
+    #Champion lane
+    #Champion damage type
+    #Other?
+    champion = 'name'
+    lane = 'mid/bot/top/jungle'
+    damage = 'ap/ad/hybrid'
+    role = 'damage/zero_cs/tank/bruiser(damage+tank)/dmg_zero_cs'#super reduced to start with
+
+
+
+class game_info:
+    #needs to have:
+    #team that won
+    #Per team:
+    #"high level team comp"
+    #type of each champion?
+    #to start super high level
+    win = -1
+    blue_comp = COMP_UNKNOWN
+    red_comnp = COMP_UNKNOWN
+
+def proccess_game(match, t1, t2):
+    do='stuff'
 
 #Processes all seed data.
-def process_seed_data():
-    with open('matches1.json') as json_file:
-        matches = json.load(json_file, 'latin-1')
+def process_seed_data(matches):
     for match in matches['matches']:
         #for each match print each champion in team and
         t1 = []
